0e029800c0ee7ab91b044be5144f10fd9697146e
Carly Ciricillo <carly.ciricillo@wunderkind.co>
2025-01-08 20:42:49 -0500

    Education update

--- scripts/sitewide-objs.js
+++ scripts/sitewide-objs.js
@@ -73,6 +73,10 @@ const mySkills = {
             "icon": "Salesforce.png"
         },
         {
+            "label": "Braze",
+            "icon": "Braze.png"
+        },
+        {
             "label": "Litmus",
             "icon": "Litmus.png"
         },
@@ -109,8 +113,16 @@ const mySkills = {
             "icon": "jQuery.png"
         },
         {
+            "label": "Handlebars",
+            "icon": "Handlebars.png"
+        },
+        {
             "label": "Storybook",
             "icon": "Storybook.png"
+        },
+        {
+            "label": "Jira",
+            "icon": "Jira.png"
         },
         {
             "label": "Git",
@@ -121,24 +133,8 @@ const mySkills = {
             "icon": "NodeJS.png"
         },
         {
-            "label": "Handlebars",
-            "icon": "Handlebars.png"
-        },
-        {
-            "label": "Jira",
-            "icon": "Jira.png"
-        },
-        {
-            "label": "TypeScript",
-            "icon": "TypeScript.png"
-        },
-        {
             "label": "React.js",
             "icon": "React.png"
-        },
-        {
-            "label": "Jest",
-            "icon": "Jest.png"
         },
         {
             "label": "Go",
@@ -153,7 +149,7 @@ const myCreds = {
             "copy1": "Bachelor of Arts",
             "copy2": "Film & Sociology",
             "copy3": "Wesleyan University",
-            "copy4": "2011-2015",
+            "copy4": "2015",
             "url": "https://www.wesleyan.edu/cfilm/",
             "icon": `<path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />`
         },
@@ -161,7 +157,7 @@ const myCreds = {
             "copy1": "Bootcamp",
             "copy2": "JavaScript ES6",
             "copy3": "General Assembly",
-            "copy4": "Summer 2019",
+            "copy4": "2019",
             "url": "https://generalassemb.ly/education/javascript-development/new-york-city",
             "icon": `<path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />`
         },
@@ -169,7 +165,7 @@ const myCreds = {
             "copy1": "Certificate",
             "copy2": "Email Development",
             "copy3": "Litmus",
-            "copy4": "Fall 2020",
+            "copy4": "2020",
             "url": "https://www.litmus.com/blog/the-first-litmus-email-marketing-certification-pros",
             "icon": `<path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />`
         },
@@ -177,7 +173,7 @@ const myCreds = {
             "copy1": "Graduate Certificate",
             "copy2": "Front End Development",
             "copy3": "Harvard Extension School",
-            "copy4": "2022 - Present",
+            "copy4": "2024",
             "url": "https://extension.harvard.edu/academics/programs/front-end-web-development-certificate/",
             "icon": `<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />`
         }
